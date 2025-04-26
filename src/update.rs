b2a79304ae3e4d1a3f9632887e7a6b7b252a441b
tekunogosu <tekunogosu@eldritchhollows.com>
2025-04-26 15:41:54 -0700

    Added url parsing for the download url. some files contain a space and will fail without encoding.

--- src/update.rs
+++ src/update.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
@@ -6,34 +7,38 @@ use crate::sync::parse_sync_file;
 use crate::utils::{dlog, RustiqueOptions};
 use rayon::prelude::*;
 use std::process::exit;
+use url::{form_urlencoded, Url};
 
 pub fn update(rustique_options: RustiqueOptions) -> Result<(), Box<dyn std::error::Error>> {
     let sync_data  = parse_sync_file(rustique_options.mod_dir.clone().unwrap());
-
     if sync_data.is_ok() {
         let sync_data = sync_data?;
 
         sync_data.rustique_sync.par_iter().for_each(|(mod_id, mod_sync_info)| {
 
             if mod_sync_info.latest_known_version != mod_sync_info.installed_version {
-                let response = ApiClient::new().download_mod(&*mod_sync_info.latest_download_url);
+                let url = Url::parse(mod_sync_info.latest_download_url.as_str()).unwrap();
+                dlog(format!("Trying to download url: {}", url.clone().to_string()).as_str());
+                let response = ApiClient::new().download_mod(&url.to_string());
 
                 match response {
                     Ok(result) => {
                         let mut bytes: Vec<u8> = Vec::new();
                         if let Ok(_) = result.into_body().into_reader().read_to_end(&mut bytes) {
-                            let file_path = rustique_options.mod_dir.clone().unwrap().join(&*mod_sync_info.latest_download_url.split('=').last().unwrap());
+                            let file_path = rustique_options.mod_dir.clone().unwrap().join(&mod_sync_info.latest_download_url.split('=').last().unwrap());
                             // create the file and write the bytes to it
-                            let path = Path::new(&file_path);
+                            let filename_fix = file_path.to_string_lossy().replace(" ", "_");
+                            let path = Path::new(&filename_fix);
                             if let Ok(mut file) = File::create(path) {
                                 if let Ok(_) = file.write_all(&bytes) {
-                                    dlog(format!("File downloaded to {}", file_path.display().to_string()).as_str());
+                                    dlog(format!("File downloaded to {}", file_path.display()).as_str());
                                 }
                             }
                         }
                     }
-                    _ => {
+                    Err(e) => {
                         dlog(format!("Something went wrong while downloading mod {}", mod_id.to_string()).as_str());
+                        dlog(format!("{:?}", e).as_str());
                     }
                 }
             }
